51bb44f96f583da3476ed3901ba19b30ba78788a
Shehan YJOZ <111506138+ShehanYjoz@users.noreply.github.com>
2022-10-10 10:47:16 +0400

    local added to repo

--- lib/src/data/repo.dart
+++ lib/src/data/repo.dart
@@ -2,7 +2,7 @@ import 'package:twilio_phone_verify/src/model/email_channel_configuration.dart';
 import 'package:twilio_phone_verify/src/model/twilio_reponse.dart';
 
 abstract class TwilioVerifyRepository {
-  Future<TwilioResponse> sendSmsCode(String phone);
+  Future<TwilioResponse> sendSmsCode(String phone, {String? locale = "en"});
   Future<TwilioResponse> verifySmsCode(String phone, String code,
       {String? locale = "en"});
   Future<TwilioResponse> sendEmailCode(String email,{EmailChannelConfiguration? channelConfiguration});
